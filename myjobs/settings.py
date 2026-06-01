9c7637475811b4bdd4974b7fe9cade89af1da7e7
muradbaksh <mdmurad8363@gmail.com>
2026-06-01 17:48:06 +0600

    Update settings.py

--- myjobs/settings.py
+++ myjobs/settings.py
@@ -125,18 +125,18 @@ LOGGING = {
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
-#     'default': dj_database_url.config(
-#         default=env('DATABASE_URL'),
-#     )
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=env('DATABASE_URL'),
+    )
+}
 
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
