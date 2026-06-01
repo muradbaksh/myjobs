0e5f74ac6f75120ccc92d5a23ac2c48152e2cfdc
muradbaksh <mdmurad8363@gmail.com>
2026-06-01 20:33:17 +0600

    Update settings.py

--- myjobs/settings.py
+++ myjobs/settings.py
@@ -33,6 +33,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
+CSRF_TRUSTED_ORIGINS = ['https://capitalbank.onrender.com','https://*.127.0.0.1']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = env.bool("DEBUG", default=False)
